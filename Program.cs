e9fcc0ede1f25f4e240cdc8a4d50e6aa82b3e157
hakyung <hageung0910@gmail.com>
2024-03-06 19:42:51 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace real_gittest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
